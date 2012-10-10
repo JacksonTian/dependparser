b9ba52a4c3a48cc40596d99599a1f3a896693810
tianqi <xinyu198736@gmail.com>
2012-10-10 17:54:36 +0800

    格式修正

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
 
-var walker=require("walkdo")
-var queue_do=require("queuedo")
-var fs=require("fs")
+var walker = require("walkdo")
+var queue_do = require("queuedo")
+var fs = require("fs")
 //带有系统模块的依赖列表
-var requireList=[]
+var requireList = []
 //最终的依赖列表
-var dependList=[]
-var depends={
+var dependList = []
+var depends = {
     
 }
 var packageInfo;
-var reg=new RegExp("require\\([\"']([^./]*?)[\"']\\)","g")
+var reg = new RegExp("require\\([\"']([^./]*?)[\"']\\)","g")
 //系统模块，take from http://nodejs.org/api/index.json
-var sysList=[ "synopsis", "assert", "buffer", "addons", "child_process", "cluster", "crypto", "debugger", "dns", "domain", "events", "fs", "globals", "http", "https", "modules", "net", "os", "path", "process", "punycode", "querystring", "readline", "repl", "stdio", "stream", "string_decoder", "timers", "tls", "tty", "dgram", "url", "util", "vm", "zlib"]
+var sysList = [ "synopsis", "assert", "buffer", "addons", "child_process", "cluster", "crypto", "debugger", "dns", "domain", "events", "fs", "globals", "http", "https", "modules", "net", "os", "path", "process", "punycode", "querystring", "readline", "repl", "stdio", "stream", "string_decoder", "timers", "tls", "tty", "dgram", "url", "util", "vm", "zlib"]
 
 
 //解析文件
-var parseJsFile=function(_file,next,context){
-    var text=fs.readFileSync(_file).toString().replace(/\s/,"")
+var parseJsFile = function(_file,next,context){
+    var text = fs.readFileSync(_file).toString().replace(/\s/,"")
     var match
-    while(match=reg.exec(text)){
+    while(match = reg.exec(text)){
         requireList.push(match[1])
     }
 }
 //去重
-var uniq=function(arr){
-    var newArray=[];
-    var provisionalTable = {};
-    for (var i = 0, item; (item= arr[i]) != null; i++) {
+var uniq = function(arr){
+    var newArray = []; 
+    var provisionalTable  =  {};
+    for (var i  =  0, item; (item =  arr[i]) !=  null; i++) {
         if (!provisionalTable[item]) {
             newArray.push(item);
-            provisionalTable[item] = true;
+            provisionalTable[item]  =  true;
         }
     }
     return newArray;
 };
 
-var dependParser=function(source){
+var dependParser = function(source){
     if(!fs.existsSync(source+"/package.json")){
         console.log("package.json not found! broken!")
         return;
     }
-    packageInfo=JSON.parse(fs.readFileSync(source+"/package.json"))
+    packageInfo = JSON.parse(fs.readFileSync(source+"/package.json"))
     walker(source,function(f,next,context){
         if(/\.js$/.test(f)){
             parseJsFile(f,next,context)
@@ -49,19 +49,19 @@ var dependParser=function(source){
         next.call(context)
     },function(){
         //去重
-        requireList=uniq(requireList)
+        requireList = uniq(requireList)
         //过滤掉系统模块
         requireList.forEach(function(i){
-            if(sysList.indexOf(i)==-1){
+            if(sysList.indexOf(i) == -1){
                 dependList.push(i)
             }
         })
         //一次处理依赖列表里的模块，用npm info 请求其信息并解析
         queue_do(dependList,function(_mod,next,context){
-            depends[_mod]=""
+            depends[_mod] = ""
             console.log("request "+_mod+"'s info")
-            var exec = require('child_process').exec,
-            free  = exec("npm info "+_mod,function(err,out,stderr){
+            var exec  =  require('child_process').exec,
+            free   =  exec("npm info "+_mod,function(err,out,stderr){
                 if(err){
                     console.log("child_process exec error！")
                 }else
@@ -70,9 +70,9 @@ var dependParser=function(source){
                     console.log(stderr)
                 }else{
                     console.log("parsing "+_mod+"'s info")
-                    var nameReg=/name\s*:\s*'(.*)'/
-                    var versionReg=/version\s*:\s*'(.*)'/
-                    depends[out.match(nameReg)[1]]=out.match(versionReg)[1]
+                    var nameReg = /name\s*:\s*'(.*)'/
+                    var versionReg = /version\s*:\s*'(.*)'/
+                    depends[out.match(nameReg)[1]] = out.match(versionReg)[1]
                 }
                 next.call(context)
             });
@@ -84,4 +84,4 @@ var dependParser=function(source){
         
     })
 }
-module.exports=exports=dependParser;
+module.exports = exports = dependParser;
