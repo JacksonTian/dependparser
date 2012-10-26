263ccf1b1a50f3a82e27123e356164847183fedc
tianqi <xinyu198736@gmail.com>
2012-10-26 20:12:20 +0800

    错误处理和过滤注释

--- demo/demo.js
+++ demo/demo.js
@@ -1,14 +1,15 @@
 var dp = require("../lib/index.js");
-dp.process(process.cwd().replace(/test$/,""), {
+dp.process(process.cwd().replace(/demo$/,""), {
     blackList:[
-        "*test.js"
+       // "*demo.js"
     ],
-    showError:true,
     checkVersion:true
 }, function (error,result) {
-    if(!error){
-        console.log(result)
+    if(error){
+        //code=1 是致命错误，code=2 是请求错误，不致命。
+         console.log(error.code)
+        console.log(error.message)
     }else{
-        console.log(error)
+        console.log(result) 
     }
 });
