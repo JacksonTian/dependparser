5ef5a0cbd6b34ec0ee7240578e43eeeba6a3b095
tianqi <xinyu198736@gmail.com>
2012-10-26 20:17:15 +0800

    错误处理和过滤注释,demo和readme调整

--- demo/demo.js
+++ demo/demo.js
@@ -7,9 +7,9 @@ dp.process(process.cwd().replace(/demo$/,""), {
 }, function (error,result) {
     if(error){
         //code=1 是致命错误，code=2 是请求错误，不致命。
-         console.log(error.code)
-        console.log(error.message)
+         console.log(error.code);
+        console.log(error.message);
     }else{
-        console.log(result) 
+        console.log(result); 
     }
 });
